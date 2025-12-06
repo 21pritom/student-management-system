bfd8b8b16e63e5c7bf45834d9ecfc1af0b68b27d
21pritom <117522593+21pritom@users.noreply.github.com>
2025-12-07 00:51:13 +0600

    first commit

--- src/app/(deshboard)/layout.tsx
+++ src/app/(deshboard)/layout.tsx
@@ -7,7 +7,7 @@ export default function deshboardLayout({
     <html lang="en">
       <div className='h-screen flex'>
         {/* *Left */}
-        <div className="w-[14%] md:w[8%] lg:w[16%] bg-red-200">r</div>
+        <div className="w-[14%] md:w[8%] xl:w[14%] bg-red-200">r</div>
          {/* *Right */}
         <div className="w-[86%] bg-blue-200" >g</div>
 
