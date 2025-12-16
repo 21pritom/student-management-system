a9c3406e774cee908436a72b343222bb50ebe3bd
Muhammad Naimul Hoque <naimulhoq89@gmail.com>
2025-12-16 17:55:03 +0600

    student is designed&bigCalender is added

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,7 +1,6 @@
+import { role } from "@/lib/data";
 import Image from "next/image";
-import Link from "next/link"; 
-
-
+import Link from "next/link";
 
 const menuItems = [
   {
@@ -120,19 +119,30 @@ const menuItems = [
 //all menu items soow
 const Menu = () => {
   return (
-    <div className="">{menuItems.map(i=>(
-      <div className="" key={i.title}>
-        <span className="hidden lg:block text-gray-400 font-light my-4">{i.title}</span>
-        {i.items.map(item=>(
-          //  ei nicar line a menu castomize like licon er text colay jabay jodi code soto hoi
-          <Link href={item.href} key={item.label} className="flex items-center justify-center lg:justify-start gap-3 p-4 text-gray-500 py-2">
-           <Image src={item.icon} alt="" width={20} height={20}/>
-            <span className="hidden lg:block">{item.label}</span>
-          </Link>
-        ))}
-      </div>
-    ))}
+    <div className="">
+      {menuItems.map((i) => (
+        <div className="" key={i.title}>
+          <span className="hidden lg:block text-gray-400 font-light my-4">
+            {i.title}
+          </span>
+          {i.items.map((item) => {
+            if (item.visible.includes(role)) {
+              return (
+                //  ei nicar line a menu castomize like licon er text colay jabay jodi code soto hoi
+                <Link
+                  href={item.href}
+                  key={item.label}
+                  className="flex items-center justify-center lg:justify-start gap-3 p-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-lamaSkyLight"
+                >
+                  <Image src={item.icon} alt="" width={20} height={20} />
+                  <span className="hidden lg:block">{item.label}</span>
+                </Link>
+              );
+            }
+          })}
+        </div>
+      ))}
     </div>
-  )
-}
-  export default Menu;
+  );
+};
+export default Menu;
